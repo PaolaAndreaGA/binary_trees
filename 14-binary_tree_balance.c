75c9bc7454a50b6ffdc207273d5a14e94e9b7b07
PaolaAndreaGA <paga020964gmail.com>
2021-12-02 14:28:31 -0800

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -47,9 +47,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (!tree)
+	if (tree == NULL)
 		return (0);
 
-	return (height(tree->left) - height(tree->right));
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 
 }
